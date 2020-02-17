90e3c79096c0ac7f879823f57056891e50a35fc7
Jiaxin Sun <sun765@purdue.edu>
2020-02-16 21:20:34 -0500

    finished!

--- client/src/components/stream/StreamShow.js
+++ client/src/components/stream/StreamShow.js
@@ -1,11 +1,43 @@
 import React from 'react';
 import {fetchStream} from '../../actions/index';
 import {connect} from 'react-redux';
+import flv from 'flv.js';
 
 class StreamShow extends React.Component{
+    constructor(props){
+        super(props);
+        // what does this step do?
+        this.videoRef = React.createRef();
+    }
+
     componentDidMount(){
         this.props.fetchStream(this.props.match.params.id);
+        this.buildPlayer();
     }
+
+    componentDidUpdate(){
+        this.buildPlayer();
+    }
+
+    componentWillUnmount(){
+        this.player.destroy();
+    }
+    
+    buildPlayer(){
+        // if build before or if we haven't load the stream yet
+        if(this.player|| !this.props.stream){
+            return;
+        }
+
+        const {id} = this.props.match.params;
+        this.player = flv.createPlayer({
+            type: 'flv',
+            url: `http://localhost:8000/live/${id}.flv`
+          });
+          this.player.attachMediaElement(this.videoRef.current);
+          this.player.load();
+    }
+
     render(){
         if(!this.props.stream){
             return (
@@ -17,6 +49,7 @@ class StreamShow extends React.Component{
 
         return (
             <div>
+                <video ref ={this.videoRef} style={{width : '100%'}} controls = {true} />
                 <h1>{title}</h1>
                 <h5>{description}</h5>
             </div>
